225a44f830049446739d00909d02aecefaa25bb2
HenriqueBispo <henriquesouzabispo848@gmail.com>
2023-11-27 17:45:03 -0300

    o

---
+++ Modulo2/Tarefa2/tarefa 2/src/components/FirstComponent.jsx
@@ -0,0 +1,15 @@
+FirstComponent = () => {
+
+    return (
+
+        <div className="App">
+
+            <h1>Este h1 está no FirstComponent.jsx</h1>
+
+        </div>
+
+    )
+
+}
+
+export default FirstComponent.jsx
