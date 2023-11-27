338d008f83bcf5dfce1c8dabbd413ed00301a6d1
HenriqueBispo <henriquesouzabispo848@gmail.com>
2023-11-27 18:48:16 -0300

    o

---
+++ Modulo2/src/App.jsx
@@ -0,0 +1,30 @@
+//Import do vite || React || npm etc...
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+// Style || CSS
+
+import './App.css'
+
+// FirstCompornent
+
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  return (
+    
+    <div>
+
+      <h1>Este h1 está no App.jsx</h1>
+
+      <FirstComponent />
+
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ Modulo2/src/components/FirstComponent.jsx
@@ -0,0 +1,15 @@
+const FirstComponent = () => {
+
+    return (
+
+        <div className="App">
+
+            <h1>Este h1 está no FirstComponent.jsx</h1>
+
+        </div>
+
+    )
+
+}
+
+export default FirstComponent
